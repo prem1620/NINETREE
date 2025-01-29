c15afeeba34156101a2fe2d8e6aa895036dd25d1
prem1620 <anshumans1320@gmail.com>
2025-01-29 15:44:22 +0530

    code improved

--- code.cpp
+++ code.cpp
@@ -1,42 +1,51 @@
 //NINETREE © 2025 by Anshuman Singh is licensed under Creative Commons Attribution-ShareAlike 4.0 International
 #include<iostream>
+#include<chrono>
 using namespace std;
 int nineTree(long long N) {
     //this function takes a long long N as input and returns the integer value of its square root.
-    bool negative = false;
-    if(N < 0) {
-        negative = true;
-        N *= -1;
-    }
+
     long long temp = N;
-    long long tripletSize = 1;
-    while(temp != 0) {
-        tripletSize = tripletSize * 3;
+    long long unitSize = 1;
+    while(temp >= 9) {
+        unitSize = unitSize * 3;
         temp = temp / 9;
     }
-    long long unitSize = tripletSize / 3;
-    long long doubleunits;
+    long long doubleUnits,sum1,sum2;
     long long squareRoot = 0;
-    long long startTerm = 1;
-    temp = N;
-    while(unitSize != 0 && temp != 0) {
-        doubleunits = unitSize * 2;
-        long long sum1 = unitSize * (startTerm + unitSize - 1);
-        long long sum2 = doubleunits * (startTerm + doubleunits - 1);
+    
+    if(temp >= 4) {
+        doubleUnits = 2 * unitSize;
+        sum2 = doubleUnits * doubleUnits;
+        temp = N - sum2;
+        squareRoot = doubleUnits;
+    }
+    else if(temp >= 1) {
+        sum1 = unitSize * unitSize;
+        temp = N - sum1;
+        squareRoot = unitSize;
+    }
+    long long startTerm;
+    startTerm = 2 * squareRoot + 1;
+    unitSize = unitSize / 3;  
+    while(unitSize > 0 && temp > 0) {
+        doubleUnits = unitSize * 2;
+        sum1 = unitSize * (startTerm + unitSize - 1);
+        sum2 = doubleUnits * (startTerm + doubleUnits - 1);
         if(temp >= sum2) {
-            squareRoot += doubleunits;
+            squareRoot = squareRoot + doubleUnits;
             temp = temp - sum2; 
         }
         else if(temp >= sum1) {
-            squareRoot += unitSize;
+            squareRoot = squareRoot + unitSize;
             temp = temp - sum1;
         }
         unitSize = unitSize / 3;
-        startTerm = 1 +  (squareRoot) * 2;
+        startTerm = 1 + (squareRoot) * 2;
     }
-    if(negative) return -1 * squareRoot;
     return squareRoot;
 }
 int main() {
-
+    cout<<sqrt(1234567891)<<endl;
+    cout<<nineTree(1234567891);
 }
